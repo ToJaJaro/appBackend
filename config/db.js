4a5682b3b21a971432e90391fe41678a65b781c7
karol <karoljarosz51@gmail.com>
2024-06-22 16:23:24 +0200

    kk

--- config/db.js
+++ config/db.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+console.log('MONGODB_URL:', process.env.MONGODB_URL);
+
 mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
